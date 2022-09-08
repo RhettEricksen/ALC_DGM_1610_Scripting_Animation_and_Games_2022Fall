8057d5bd5b37dabb437cacea6d7ad69754916f31
Rhett <rhett.e312@stu.nebo.edu>
2022-09-08 09:19:47 -0600

    New Project

---
+++ Balloon POP Game/Assets/Scripts/Balloon.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    //public int scoreToGive = 100; // Score give for popped balloon
+
+    public int clickToPop = 3; // How many clicks before balloon pops
+
+    public float scaleToIncrease = 0.10f; //Scale increase each time the balloon is clicked
+
+    //public ScoreManager scoeManager; // A variable to refernce the ScoreManager
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Reference ScoreManager Component
+        //scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
+
+    void OnMouseDown()
+    {
+        // Reduce clicks by one
+        clickToPop -= 1;
+
+        //Increase balloon volume
+        transform.localScale += Vector3.one * scaleToIncrease;
+
+        if(clickToPop ==0)
+        {
+            //scoreManager.IncreaseScoreText(scoreToGive);
+            Destroy(gameObject);
+
+        }
+    }
+}
