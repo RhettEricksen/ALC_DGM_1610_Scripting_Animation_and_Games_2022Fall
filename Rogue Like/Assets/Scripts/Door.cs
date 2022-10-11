c233020815474858717ee4b005393184316214df
Rhett <rhett.e312@stu.nebo.edu>
2022-10-11 09:18:28 -0600

    door mechanic

---
+++ Rogue Like/Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{   
+    private GameManager gm;
+    public float doorDelay = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();    
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player") && gm.key >= 1)
+        { 
+            Destroy(gameObject, doorDelay);
+            gm.key --;
+            Debug.Log("Keys =" + gm.key);
+            Debug.Log("Door is open!");
+        }
+        else
+        {
+            Debug.Log("Door is Locked. You need a key");
+        }
+    }
+}
